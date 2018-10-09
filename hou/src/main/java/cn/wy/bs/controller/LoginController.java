f3e0da0780a7705353ca7226c3e83ddc476d6576
wangningboya <843096638@qq.com>
2018-10-09 09:51:39 +0800

    no message

--- hou/src/main/java/cn/wy/bs/controller/LoginController.java
+++ hou/src/main/java/cn/wy/bs/controller/LoginController.java
@@ -21,29 +21,35 @@ public class LoginController {
     @Resource
     private UserService userService;
 
+    @RequestMapping(value = "/test")
+    public String test(){
+        System.out.println("testttttt");
+        return "aaa";
+    }
 
     @RequestMapping(value = "/login")
     public ResponseData login(
-            HttpServletRequest request,
-            @RequestBody HashMap<String,Object> map
+            HttpServletRequest request
+//            @RequestBody HashMap<String,Object> map
     ){
         ResponseData responseData=new ResponseData();
         JSONObject jsonObject = new JSONObject();
-        int count=userService.findByUserNameAndPassword(map);
-        switch (count){
-            case 0:
-                responseData.setRspCode("100001");
-                responseData.setRspMsg("账号或者密码错误！");
-                break;
-            case 1:
-                HttpSession session =request.getSession();
-                session.setAttribute("userName",map.get("username"));
-                jsonObject.put("userName",session.getAttribute("userName"));
-                responseData.setData(jsonObject);
-                responseData.setRspCode("000000");
-                responseData.setRspMsg("登录成功");
-                break;
-        }
+        System.out.println("login");
+//        int count=userService.findByUserNameAndPassword(map);
+//        switch (count){
+//            case 0:
+//                responseData.setRspCode("100001");
+//                responseData.setRspMsg("账号或者密码错误！");
+//                break;
+//            case 1:
+//                HttpSession session =request.getSession();
+//                session.setAttribute("userName",map.get("username"));
+//                jsonObject.put("userName",session.getAttribute("userName"));
+//                responseData.setData(jsonObject);
+//                responseData.setRspCode("000000");
+//                responseData.setRspMsg("登录成功");
+//                break;
+//        }
         return responseData;
     }
 }
